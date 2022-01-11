aacfa7836444e3ea8f5728dcc69c60863c3ff077
KatUser <ekaterina.shirokikh@gmail.com>
2022-01-11 10:32:20 +0700

    updated test_items

--- test_items.py
+++ test_items.py
@@ -4,10 +4,10 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-def test_language_link(browser):
+def test_if_the_cart_button_exists(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
-    cart_button = WebDriverWait(browser, 30).until(EC.presence_of_element_located((By.CSS_SELECTOR, '#add_to_basket_form > button')))
+    WebDriverWait(browser, 30).until(EC.presence_of_element_located((By.CSS_SELECTOR, '#add_to_basket_form > button')))
     assert browser.find_element(By.CSS_SELECTOR, '#add_to_basket_form > button').size != 0, f'No cart found'
 
 
